3b6edb94e9d14f40a0f8ec73df01f41fe4a621d2
Rafa Bernal <calmayourdrama@proton.me>
2024-08-13 10:43:26 +0200

    remove example component

--- apps/web/src/app/app.component.ts
+++ apps/web/src/app/app.component.ts
@@ -1,14 +1,14 @@
-import { Component } from '@angular/core';
-import { RouterModule } from '@angular/router';
-import { NxWelcomeComponent } from './nx-welcome.component';
+import { ChangeDetectionStrategy, Component } from '@angular/core'
+import { RouterModule } from '@angular/router'
 
 @Component({
   standalone: true,
-  imports: [NxWelcomeComponent, RouterModule],
+  imports: [RouterModule],
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class AppComponent {
-  title = 'web';
+  title = 'web'
 }
